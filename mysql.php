fec23243bad7115cae6a2e67e76b6a95ced1f7e5
Atulya Kushwaha <atulya1234567890@gmail.com>
2025-03-13 02:41:25 +0900

    add new feature

--- mysql.php
+++ mysql.php
@@ -13,17 +13,36 @@
         <input type="text"name="dep"></input>
         <button type="submit"name="submit">submit</button>
      
+   
 
+</form>
+<form method="post" action="mysql.php">
+      <button type="submit" name="record">show record</button>
 </form>
 
     <?php
+    if(isset($_POST["record"])){
+       $mycom=mysqli_connect("localhost:4306","root","","collage");
+       $sql="select*from emp";
+       $record=$mycom->query($sql);
+       $n=mysqli_num_rows($record);
+       if($n>0){
+        while($row=mysqli_fetch_row($record)){
+            echo $row[0]."".$row[1]."".$row[2]."".$row[3];
+            echo "<br>";
+        }
+       }
+       else
+        echo "<font color=red>no record</font>";
+
+    }
     if(isset($_POST["submit"])){
      $eid=$_POST["empid"];
      $name=$_POST["name"];
      $sal=$_POST["salary"];
      $dep=$_POST["dep"];
-     $mycom=mysqli_connect("localhost","root","12345","collage");
-     echo "connection sucess";
+     $mycom=mysqli_connect("localhost:4306","root","","collage");
+    
      $sql="insert into emp values(?,?,?,?)";
      $ps=$mycom->prepare($sql);
      $ps->bind_param("isis",$eid,$name,$sal,$dep);
