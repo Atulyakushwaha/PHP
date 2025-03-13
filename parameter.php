2952e840d7b45c2c7232a6136c80aefc4313d993
Atulya Kushwaha <atulya1234567890@gmail.com>
2025-03-14 07:04:22 +0900

    paramter

---
+++ parameter.php
@@ -0,0 +1,13 @@
+<?php
+function asd(){
+    return "atulya";
+}
+asd();
+//def
+function fruit($name  , $color="red"){
+    return "the $name having color $color";
+}
+$prin = fruit("apple","green");
+echo "".$prin;
+
+?>
